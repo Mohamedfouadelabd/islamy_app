a20836bc542e0312b50096a16149b848c5515719
mmido <mf1579295@gmail.com>
2025-07-18 01:02:55 +0300

    Home screen Provider in main

--- lib/main.dart
+++ lib/main.dart
@@ -1,15 +1,22 @@
 import 'package:flutter/material.dart';
 import 'package:islamy_project/Home_Screen.dart';
+import 'package:islamy_project/Provider/app_config_provider.dart';
 import 'package:islamy_project/Theme/my_theme.dart';
 import 'package:flutter_gen/gen_l10n/app_localizations.dart';
+import 'package:provider/provider.dart';
 void main() {
-  runApp(MyApp());
+  runApp(ChangeNotifierProvider(
+      create: (context) => AppConfigProvider(),
+      child: MyApp()));
 }
 
 class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
+    var provider =Provider.of<AppConfigProvider>(context);
     return MaterialApp(
+     themeMode: provider.appTheme,
+      locale: Locale(provider.appLanguage),
       localizationsDelegates: AppLocalizations.localizationsDelegates,
       supportedLocales: AppLocalizations.supportedLocales,
 
